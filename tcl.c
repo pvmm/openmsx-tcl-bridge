2eaece30f3d78171c4221e405e0e78e6695db97e
Pedro de Medeiros <pedro.medeiros@gmail.com>
2025-10-03 12:27:36 -0300

    first commit

---
+++ tcl.c
@@ -0,0 +1,37 @@
+#include "msxgl.h"
+#include "dos.h"
+
+// Send command to Tcl engine
+bool tcl(char* s)
+{
+    s; // hl <- s
+    __asm
+        // write address
+        ld c, #6
+        out (c), l
+        out (c), h
+        // read response
+        ld h, #0
+        ld c, #7
+        in l, (c)
+    __endasm;
+    return s == 0;
+}
+
+void main(u8 argc, c8** argv)
+{
+    if (argc < 1) {
+        DOS_StringOutput("tcl v1 (c) 2025 Pedro de Medeiros\r\n$");
+        DOS_StringOutput("No arguments provided.$");
+        DOS_Exit0();
+    }
+
+    // Concatenate all command line parameters
+    for (u8 i = 0; i < argc; i++) {
+        argv[i][-1] = ' ';
+    }
+
+    DOS_StringOutput(tcl(argv[0]) ? "OK$" : "ERROR$");
+    DOS_Exit0();
+}
+
