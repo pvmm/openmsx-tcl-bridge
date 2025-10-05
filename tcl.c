575da085f59683b69e18a7c6bc18c362a6b9b101
Pedro de Medeiros <pedro.medeiros@gmail.com>
2025-10-05 12:18:44 -0300

    Sync recent changes in tcl_bridge

--- tcl.c
+++ tcl.c
@@ -4,13 +4,16 @@
 
 // Data to send to tcl_bridge
 struct {
-    c8* output;
-    c8* input;
-    u16 size;
+    c8* output;         // offset + 0
+    u16 output_size;    // offset + 2
+    c8* input;          // offset + 4
+    u16 input_size;     // offset + 6
+    u16 input_max;      // offset + 8
+    i8  status;         // offset + 10
 } tcl_data;
 
-#define SIZE 1000
-c8 input[SIZE] = "tcl_bridge not running.$";
+#define MAX 1000
+c8 input[MAX] = "tcl_bridge not running.$";
 
 // Send command to Tcl engine
 c8* tcl(void* data)
@@ -22,26 +25,36 @@ c8* tcl(void* data)
         out (c), l
         out (c), h
     __endasm;
-    return &input;
+    return input;
+}
+
+unsigned int strlen(c8* s)
+{
+    unsigned int i = 0;
+    for (i = 0; s[i] != 0; ++i);
+    return i;
 }
 
 void main(u8 argc, c8** argv)
 {
     if (argc < 1) {
         DOS_StringOutput("tcl.com v1.0 (c) 2025 Pedro de Medeiros\r\n$");
-        DOS_StringOutput("No arguments provided.$");
+        DOS_StringOutput("No arguments $$ provided.$");
         DOS_Exit0();
     }
 
-    // Concatenate all command line parameters
+    // concatenate all command line parameters
     for (u8 i = 0; i < argc; i++) {
         argv[i][-1] = ' ';
     }
 
-    tcl_data.output = argv[0];
-    tcl_data.input  = input;
-    tcl_data.size   = SIZE;
-    DOS_StringOutput(tcl(&tcl_data));
+    tcl_data.output      = argv[0];
+    tcl_data.output_size = strlen(argv[0]);
+    tcl_data.input       = input;
+    tcl_data.input_max   = MAX;
+    c8* s = tcl(&tcl_data); // here tcl_data fields are magically filled in...
+    s[tcl_data.input_size] = '$';
+    DOS_StringOutput(s);
     DOS_Exit0();
 }
 
