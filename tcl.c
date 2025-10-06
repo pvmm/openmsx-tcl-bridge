64bb392c317104404caca5cba1b0cf5a9a136296
Pedro de Medeiros <pedro.medeiros@gmail.com>
2025-10-05 22:15:32 -0300

    Swap positions of output_size and output_max

--- tcl.c
+++ tcl.c
@@ -5,14 +5,15 @@
 
 #include "msxgl.h"
 #include "dos.h"
+#include "string.h"
 
 // Data to send to tcl_bridge
 struct {
     c8* output;         // offset + 0
     u16 output_size;    // offset + 2
     c8* input;          // offset + 4
-    u16 input_size;     // offset + 6
-    u16 input_max;      // offset + 8
+    u16 input_max;      // offset + 6
+    u16 input_size;     // offset + 8
     i8  status;         // offset + 10
 } tcl_data;
 
@@ -20,7 +21,7 @@ struct {
 c8 input[MAX] = "tcl_bridge not running.$";
 
 // Send command to Tcl engine
-c8* tcl(void* data)
+void tcl(void* data)
 {
     data; // assumption: hl <- data
     __asm
@@ -29,14 +30,6 @@ c8* tcl(void* data)
         out (c), l
         out (c), h
     __endasm;
-    return input;
-}
-
-unsigned int strlen(c8* s)
-{
-    unsigned int i = 0;
-    for (i = 0; s[i] != 0; ++i);
-    return i;
 }
 
 void main(u8 argc, c8** argv)
@@ -53,12 +46,12 @@ void main(u8 argc, c8** argv)
     }
 
     tcl_data.output      = argv[0];
-    tcl_data.output_size = strlen(argv[0]);
+    tcl_data.output_size = String_Length(argv[0]);
     tcl_data.input       = input;
     tcl_data.input_max   = MAX;
-    c8* s = tcl(&tcl_data); // here tcl_data fields are magically filled in...
-    s[tcl_data.input_size] = '$';
-    DOS_StringOutput(s);
+    tcl(&tcl_data); // here tcl_data fields are magically filled in...
+    input[tcl_data.input_size] = '$';
+    DOS_StringOutput(input);
     DOS_Exit0();
 }
 
